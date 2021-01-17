a7dade4557f15e48ce329e505153f8e4a1b42886
kingapl <kingapluciennik@wp.pl>
2021-01-17 13:09:40 +0100

    rock paper scissors game

---
+++ rock_paper_scissors.py
@@ -0,0 +1,52 @@
+from random import randrange
+
+game = True
+symbols = ['rock', 'paper', 'scissors']
+
+
+def user_choice():
+    u_choice = input("\nChoice rock, paper or scissors and press enter.\n")
+
+    while u_choice != 'rock' and u_choice != 'paper' and u_choice != 'scissors':
+        u_choice = input("Enter the correct name 'rock', 'paper' or 'scissors'.\n")
+
+    return u_choice
+
+
+def computer_choice():
+    s_num = randrange(len(symbols))
+    com_choice = symbols[s_num]
+
+    return com_choice
+
+
+def result(u_choice, com_choice):
+    if ((u_choice == 'rock' and com_choice == 'rock') or
+        (u_choice == 'paper' and com_choice == 'paper') or
+        (u_choice == 'scissors' and com_choice == 'scissors')):
+            return "Dead-heat"
+
+    elif ((u_choice == 'rock' and com_choice == 'scissors') or
+        (u_choice == 'paper' and com_choice == 'rock') or
+        (u_choice == 'scissors' and com_choice == 'paper')):
+            return "You win!"
+
+    elif ((u_choice == 'rock' and com_choice == 'paper') or
+        (u_choice == 'paper' and com_choice == 'scissors') or
+        (u_choice == 'scissors' and com_choice == 'rock')):
+            return "You lost!"
+
+
+while game == True:
+    u_choice = user_choice()
+    com_choice = computer_choice()
+    game_result = result(u_choice, com_choice)
+
+    print(f"\nYour choice: {u_choice}.")
+    print(f"Computer choice: {com_choice}.")
+    print(f"\n{game_result}")
+
+    answer = input(f"\nDo you want to play again? y/n ")
+
+    if answer.lower() != 'y':
+        game = False
