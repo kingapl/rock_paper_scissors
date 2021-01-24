9f706669020c924f7d814b3eb3ab0244bf42287d
kingapl <kingapluciennik@wp.pl>
2021-01-24 21:14:52 +0100

    Added display of computer choice and labels for choices and result.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -8,38 +8,52 @@ class RockPaperScissors(tkinter.Frame):
         self.root = root
         self.pack()
 
-        root.geometry("400x400")
-        root.title("Rock Paper Scissors")
+        self.root.geometry("400x400")
+        self.root.title("Rock Paper Scissors")
 
         self.create_widgets()
 
 
     def create_widgets(self):
         self.game_name = tkinter.Label(text="Rock Paper Scissors", font="courier 24")
-        self.game_name.pack()
+        self.game_name.pack(side="top")
 
         self.instruction_text = tkinter.Label(text="Choose rock, paper or scissors", font="Arial 12", height=3)
-        self.instruction_text.pack()
+        self.instruction_text.pack(side="top")
+
+        self.user_label = tkinter.Label(text="Your choice:")
+        self.user_label.pack()
 
         self.user_choice = tkinter.StringVar()
         self.user_entry = tkinter.Entry(textvariable=self.user_choice, font="Arial 12")
         self.user_entry.pack()
 
-        self.play_btn = tkinter.Button(root, text="Play", command=self.play, font="Arial 14")
+        self.play_btn = tkinter.Button(text="Play", command=self.play, font="Arial 14")
         self.play_btn.pack()
 
-        self.game_result = tkinter.StringVar()
-        self.computer_entry = tkinter.Entry(root, text=self.game_result, font="Arial 12")
+        self.computer_label = tkinter.Label(text="Computer choice:")
+        self.computer_label.pack()
+
+        self.computer_choice = tkinter.StringVar()
+        self.computer_entry = tkinter.Entry(text=self.computer_choice, font="Arial 12")
         self.computer_entry.pack()
 
-        self.reset_btn = tkinter.Button(root, text="Reset", command=self.reset, font="Arial 14")
-        self.reset_btn.pack(side="bottom")
+        self.result = tkinter.Label(text="Result:")
+        self.result.pack()
+
+        self.game_result = tkinter.StringVar()
+        self.result_entry = tkinter.Entry(text=self.game_result, font="Arial 12")
+        self.result_entry.pack()
+
+        self.reset_btn = tkinter.Button(text="Reset", command=self.reset, font="Arial 14")
+        self.reset_btn.pack()
 
 
     def play(self):
         user_pick = self.user_choice.get()
         symbols = ["rock", "paper", "scissors"]
         comp_choice = choice(symbols)
+        self.computer_choice.set(comp_choice)
 
         if ((user_pick == 'rock' and comp_choice == 'rock') or
             (user_pick == 'paper' and comp_choice == 'paper') or
@@ -60,6 +74,7 @@ class RockPaperScissors(tkinter.Frame):
     def reset(self):
         self.user_entry.delete(0, 'end')
         self.computer_entry.delete(0, 'end')
+        self.result_entry.delete(0, 'end')
 
 
 root = tkinter.Tk()
