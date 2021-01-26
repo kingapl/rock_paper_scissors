cd9f4f412fe29c8974f53abac2dbd8dce1974ece
kingapl <kingapluciennik@wp.pl>
2021-01-26 22:20:47 +0100

    Handling exception: empty 'user_entry' or bad value.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -30,52 +30,66 @@ class RockPaperScissors(tkinter.Frame):
                                         width=12, font="Arial 12")
         self.user_entry.grid(row=2, column=1, sticky="w", padx=5)
 
+        self.info_label = tkinter.Label(self, text=" ", 
+                                        font="Arial 10", fg="#b51818")
+        self.info_label.grid(row=3, column=0, columnspan=2)
+
         self.play_btn = tkinter.Button(self, text="PLAY", command=self.play, 
                                         font="Arial 14", bg="#ffb829")
-        self.play_btn.grid(row=3, column=0, columnspan=2, pady=20)
+        self.play_btn.grid(row=4, column=0, columnspan=2, pady=20)
 
         self.computer_label = tkinter.Label(self, text="Computer choice:", 
                                         font="Arial 10")
-        self.computer_label.grid(row=4, column=0, sticky="e", padx=5, pady=10)
+        self.computer_label.grid(row=5, column=0, sticky="e", padx=5, pady=10)
 
         self.computer_choice = tkinter.StringVar()
         self.computer_entry = tkinter.Entry(self, text=self.computer_choice, 
                                         width=12, font="Arial 12")
-        self.computer_entry.grid(row=4, column=1, sticky="w", padx=5)
+        self.computer_entry.grid(row=5, column=1, sticky="w", padx=5)
 
-        self.result = tkinter.Label(self, text="Result:", font="Arial 10")
-        self.result.grid(row=5, column=0, columnspan=2, pady=10)
+        self.result_label = tkinter.Label(self, text="Result:", font="Arial 10")
+        self.result_label.grid(row=6, column=0, columnspan=2, pady=10)
 
-        self.game_result = tkinter.StringVar()
-        self.result_entry = tkinter.Entry(self, text=self.game_result, 
+        self.result = tkinter.StringVar()
+        self.result_entry = tkinter.Entry(self, text=self.result, 
                                         font="Arial 12", justify="center")
-        self.result_entry.grid(row=6, column=0, columnspan=2)
+        self.result_entry.grid(row=7, column=0, columnspan=2)
 
         self.reset_btn = tkinter.Button(self, text="Reset", command=self.reset,
                                         font="Arial 14", bg="#ffb829")
-        self.reset_btn.grid(row=7, column=0, columnspan=2, pady=20)
+        self.reset_btn.grid(row=8, column=0, columnspan=2, pady=20)
 
 
     def play(self):
-        user_pick = self.user_choice.get()
-        symbols = ["rock", "paper", "scissors"]
-        comp_choice = choice(symbols)
-        self.computer_choice.set(comp_choice)
-
-        if ((user_pick == 'rock' and comp_choice == 'rock') or
+        def verify_game_result(self):
+            if ((user_pick == 'rock' and comp_choice == 'rock') or
             (user_pick == 'paper' and comp_choice == 'paper') or
             (user_pick == 'scissors' and comp_choice == 'scissors')):
-                self.game_result.set("Dead-heat")
+                return "Dead-heat"
 
-        elif ((user_pick == 'rock' and comp_choice == 'scissors') or
-            (user_pick == 'paper' and comp_choice == 'rock') or
-            (user_pick == 'scissors' and comp_choice == 'paper')):
-                self.game_result.set("You win!")
+            elif ((user_pick == 'rock' and comp_choice == 'scissors') or
+                (user_pick == 'paper' and comp_choice == 'rock') or
+                (user_pick == 'scissors' and comp_choice == 'paper')):
+                    return "You win!"
 
-        elif ((user_pick == 'rock' and comp_choice == 'paper') or
-            (user_pick == 'paper' and comp_choice == 'scissors') or
-            (user_pick == 'scissors' and comp_choice == 'rock')):
-                self.game_result.set("You lost!")
+            elif ((user_pick == 'rock' and comp_choice == 'paper') or
+                (user_pick == 'paper' and comp_choice == 'scissors') or
+                (user_pick == 'scissors' and comp_choice == 'rock')):
+                    return "You lost!"
+
+
+        symbols = ["rock", "paper", "scissors"]
+        comp_choice = choice(symbols)
+
+        user_pick = self.user_choice.get()
+        if (user_pick == 'rock' or user_pick == 'paper' or 
+            user_pick == 'scissors'):
+            self.computer_choice.set(comp_choice)
+            self.info_label['text'] = " "
+            game_result = verify_game_result(self)
+            self.result.set(game_result)
+        else:
+            self.info_label['text'] = "Bad value or empty entry."
 
 
     def reset(self):
